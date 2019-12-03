fc6952d89cb6656985d826ff73e31339c20355d6
jimmyb2508 <james.barrington1985@gmail.com>
2019-12-03 22:51:14 +0000

    Hooked up to API end point

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -4,15 +4,36 @@ import LocationDetails from './location-details';
 import ForecastSummaries from './forecast-summaries';
 import '../styles/app.css';
 import ForecastDetails from './ForecastDetails';
+import Axios from 'axios';
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      selectedDate: this.props.forecasts[0].date,
+      selectedDate: 0,
+      forecasts: [],
+      location: {
+        city: '',
+        country: '',
+      },
     };
 
     this.handleForecastSelect = this.handleForecastSelect.bind(this);
+  }
+
+  componentDidMount() {
+    Axios.get(`https://mcr-codes-weather.herokuapp.com/forecast?city=Manchester`).then(data => {
+      const forecasts = data.data.forecasts.map(forecast => forecast);
+      const locationDetails = {
+        city: data.data.location.city,
+        country: data.data.location.country,
+      };
+
+      this.setState({
+        forecasts,
+        location: locationDetails,
+      });
+    });
   }
 
   handleForecastSelect(date) {
@@ -22,28 +43,30 @@ class App extends React.Component {
   }
 
   render() {
-    const selectedForecast = this.props.forecasts.find(forecast => forecast.date === this.state.selectedDate);
+    const selectedForecast = this.state.forecasts.find(
+      forecast => forecast.date === this.state.selectedDate,
+    );
 
-  return (
-    <div className="forecast">
-      <LocationDetails
-      city={this.props.location.city} 
-      country={this.props.location.country} 
-      />
+    return (
+      <div className="forecast">
+        <LocationDetails city={this.state.location.city} country={this.state.location.country} />
 
-      <ForecastSummaries forecasts={this.props.forecasts} onForecastSelect={this.handleForecastSelect} />
-      <ForecastDetails forecast={selectedForecast} />
-    </div>
+        <ForecastSummaries
+          forecasts={this.state.forecasts}
+          onForecastSelect={this.handleForecastSelect}
+        />
+        {selectedForecast && <ForecastDetails forecast={selectedForecast} />}
+      </div>
     );
   }
-};
+}
 
-App.propTypes = {
-  location: PropTypes.shape({
-    city: PropTypes.string,
-    country: PropTypes.string,
-  }).isRequired,
-  forecasts: PropTypes.array.isRequired,
-};
+// App.propTypes = {
+//   location: PropTypes.shape({
+//     city: PropTypes.string,
+//     country: PropTypes.string,
+//   }).isRequired,
+//   forecasts: PropTypes.array.isRequired,
+// };
 
 export default App;
